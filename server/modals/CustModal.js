69a7a743c337c8095710edc8a67a91b22e11bbe0
vaibh-shriyaan <vaibhshriyaan2710@gmail.com>
2025-07-15 16:37:23 +0530

    Update CustModal.js
    CVV type mismatch

--- server/modals/CustModal.js
+++ server/modals/CustModal.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 
 const User_Schema = new mongoose.Schema({
   Name: {  type:String,required:true},
-  Email: {  type:String,required:true,match: /.+\@.+\..+/},
+  Email: {  type:String,match: /.+\@.+\..+/},
   Course_Name: {  type:String,required:true},
   Course_id: {  type:Number,required:true},
   Phone: {  type:Number,required:true,unique:true,match: /^[0-9]{10}$/},
   Amount_paid: { type:Number,required:true},
   Access_type: {  type:String,required:true},
   Verification_value: {  type:String,required:true},
-  CVV: { type:Number,required:true,unique:true},
+  CVV: { type:String,required:true,unique:true},
   CardNumber:{ type:Number,required:true,unique:true},
-  AIRR_ID:{  type:Number,required:true}
+  AIRR_ID:{  type:String,required:true}
 },{timestamps:true});
 
 const User_data = mongoose.model("User_data",User_Schema);
